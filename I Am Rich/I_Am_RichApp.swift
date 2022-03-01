11d52e22fff9ece9d652125f24b898750e84bdef
SeongMin-K <eo1tks3@naver.com>
2022-03-01 19:59:23 +0900

    Initial Commit

---
+++ I Am Rich/I_Am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_RichApp.swift
+//  I Am Rich
+//
+//  Created by SeongMinK on 2022/03/01.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
